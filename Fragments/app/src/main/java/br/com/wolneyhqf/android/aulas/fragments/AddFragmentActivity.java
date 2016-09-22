f9b5a35102aa613c399afa169bd1892a92f8975b
Wolney Freitas <wolneyhqf@gmail.com>
2016-09-22 00:07:25 -0300

    atualizando projeto de Fragments, versão para Android Studio 2.2

--- Fragments/app/src/main/java/br/com/wolneyhqf/android/aulas/fragments/AddFragmentActivity.java
+++ Fragments/app/src/main/java/br/com/wolneyhqf/android/aulas/fragments/AddFragmentActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import br.com.wolneyhqf.android.aulas.fragments.fragments.FragmentB;
 import br.com.wolneyhqf.android.aulas.fragments.fragments.HelloWorldFragment;
 
 public class AddFragmentActivity extends AppCompatActivity {
@@ -15,6 +16,11 @@ public class AddFragmentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_fragment_xml);
 
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
+        FragmentB fragmentB = new FragmentB();
+        fragmentTransaction.add(R.id.frame_fragmentB, fragmentB, FragmentB.TAG);
+        fragmentTransaction.commit();
     }
 }
