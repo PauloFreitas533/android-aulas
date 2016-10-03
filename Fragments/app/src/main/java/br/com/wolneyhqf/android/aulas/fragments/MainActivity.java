a6de30ffe885d8d30694b8dc8149b0cd0f12f2fd
Wolney Freitas <wolneyhqf@gmail.com>
2016-10-03 17:11:03 -0300

    Exemplo de utilização da Back Stack dos Fragments

--- Fragments/app/src/main/java/br/com/wolneyhqf/android/aulas/fragments/MainActivity.java
+++ Fragments/app/src/main/java/br/com/wolneyhqf/android/aulas/fragments/MainActivity.java
@@ -59,5 +59,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button buttonActivityBackStack = (Button) findViewById(R.id.button_activity_back_stack);
+        buttonActivityBackStack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BackStackActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
