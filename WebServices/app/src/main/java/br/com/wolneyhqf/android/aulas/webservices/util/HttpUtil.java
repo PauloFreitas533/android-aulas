83a740ddf7f1f0f0a9b05ecd85e5a05c1dd21672
Wolney Freitas <wolneyhqf@gmail.com>
2016-11-25 15:55:46 -0300

    WebServices

---
+++ WebServices/app/src/main/java/br/com/wolneyhqf/android/aulas/webservices/util/HttpUtil.java
@@ -0,0 +1,47 @@
+package br.com.wolneyhqf.android.aulas.webservices.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * Created by Wolney on 25/11/2016.
+ */
+
+public class HttpUtil {
+
+    public static HttpURLConnection openConnection(String path, String httpMethod, boolean doInput, boolean doOutput){
+        try {
+            URL url = new URL(path);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(15000);
+            connection.setRequestMethod(httpMethod);
+            connection.setDoInput(doInput);
+            connection.setDoOutput(doOutput);
+            connection.connect();
+            return connection;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static boolean hasConnection(Context context){
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+}
