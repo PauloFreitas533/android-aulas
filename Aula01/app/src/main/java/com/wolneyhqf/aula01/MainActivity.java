3095a418de81871be7fd2bf430a5844a5e23eacb
Wolney Freitas <wolneyhqf@gmail.com>
2017-08-19 18:37:31 -0300

    Aula sobre activity - Unifavip

---
+++ Aula01/app/src/main/java/com/wolneyhqf/aula01/MainActivity.java
@@ -0,0 +1,75 @@
+package com.wolneyhqf.aula01;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText campoDeTextNome;
+    private Button botaoEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        campoDeTextNome = (EditText) findViewById(R.id.campo_de_texto_nome);
+        botaoEnviar = (Button) findViewById(R.id.botao_enviar);
+
+        botaoEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nome = campoDeTextNome.getText().toString();
+                Toast.makeText(getBaseContext(), "Seu nome é: " + nome, Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(getBaseContext(), SegundaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Log.i("AppAula", "MainActivity.onCreate()");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i("AppAula", "MainActivity.onStart()");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i("AppAula", "MainActivity.onResume()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("AppAula", "MainActivity.onPause()");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("AppAula", "MainActivity.onStop()");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i("AppAula", "MainActivity.onRestart()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("AppAula", "MainActivity.onDestroy()");
+    }
+
+}
---
+++ Aula01/app/src/main/java/com/wolneyhqf/aula01/SegundaActivity.java
@@ -0,0 +1,56 @@
+package com.wolneyhqf.aula01;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by wolney on 19/08/17.
+ */
+
+public class SegundaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_segunda);
+        Log.i("AppAula", "SegundaActivity.onCreate()");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i("AppAula", "SegundaActivity.onStart()");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i("AppAula", "SegundaActivity.onResume()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("AppAula", "SegundaActivity.onPause()");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("AppAula", "SegundaActivity.onStop()");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i("AppAula", "SegundaActivity.onRestart()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("AppAula", "SegundaActivity.onDestroy()");
+    }
+
+}
